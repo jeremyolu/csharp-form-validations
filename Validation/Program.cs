5a21311f33110d017909a2bfe97cf300f5439ba4
jeremyolu <jeremy-olu@hotmail.com>
2022-01-28 09:17:05 +0000

    added examples for validation methods

--- Validation/Program.cs
+++ Validation/Program.cs
@@ -8,15 +8,23 @@ namespace Validation
         {
             var validationService = new ValidationService();
 
-            var mobile = "07865423210";
-            var email = "rosemaryallan@live.com";
+            var validMobile = "07865423210";
+            var validEmail = "rosemaryallan@live.com";
 
-            var mobileResult = validationService.IsMobileValid(mobile);
-            var emailResult = validationService.IsEmailValid(email);
+            var inValidMobile = "0785432*2-1";
+            var inValidEmail = "rosemary.allan@@live.com";
 
-            Console.WriteLine($"Mobile: {mobile} - Valid: {mobileResult}");
-            Console.WriteLine($"Email: {email} - Valid: {emailResult}");
+            var validMobileResult = validationService.IsMobileValid(validMobile);
+            var validEmailResult = validationService.IsEmailValid(validEmail);
+
+            var inValidMobileResult = validationService.IsMobileValid(inValidMobile);
+            var inValidEmailResult = validationService.IsEmailValid(inValidEmail);
+
+            Console.WriteLine($"Mobile: {validMobile} - Valid: {validMobileResult}");
+            Console.WriteLine($"Email: {validEmail} - Valid: {validEmailResult}");
+
+            Console.WriteLine($"Mobile: {inValidMobile} - Invalid: {inValidMobileResult}");
+            Console.WriteLine($"Email: {inValidEmail} - Invalid: {inValidEmailResult}");
         }
-        
     }
 }
